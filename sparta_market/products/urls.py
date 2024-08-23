2960f0e75fbf73233819712315f7e5af833b9056
feliciathebeen <feliciathebeen@gmail.com>
2024-08-23 16:36:40 +0900

    detail error

--- sparta_market/products/urls.py
+++ sparta_market/products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "products"
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('detail/', views.detail, name="detail")
 ]
--- sparta_market/products/views.py
+++ sparta_market/products/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Article
 
 
 def home(request):
-    return render(request, "products/home.html")
+    articles = Article.objects.all().order_by("-id")
+    context = {
+        "articles": articles,
+    }
+    return render(request, "products/home.html", context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        "article": article,
+    }
+    return render(request, "products/detail.html", context)
