c50340927e2e3962e93e1449f960ad10004078cc
zhddkf <25studio@naver.com>
2024-08-23 10:49:22 +0900

    +signup

--- sparta_market/accounts/views.py
+++ sparta_market/accounts/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import logout as auth_logout
 from django.views.decorators.http import require_POST, require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-# from .forms import CustomUserCreationForm
+from .forms import SignupForm
 
 
 def login(request):
@@ -25,3 +25,16 @@ def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
     return redirect("products:home")
+
+@require_http_methods(['GET','POST'])
+def signup(request):
+    if request.method=='POST':
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('products:home')
+    else :
+        form=SignupForm()
+
+    context={'form':form}
+    return render(request, 'accounts/signup.html', context)
